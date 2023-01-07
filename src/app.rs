568f5b0031e41dd7c37f7e919b2028258773089c
Jose Torres <jose@wayve.ai>
2023-01-07 17:57:31 +0000

    Try the hash router to avoid 404 problems.

--- src/app.rs
+++ src/app.rs
@@ -14,12 +14,12 @@ pub struct Props {
 pub fn app() -> Html {
     html! {
         <div class="container-fluid">
-            <BrowserRouter>
+            <HashRouter>
                 <Nav />
                 <main class="row">
                     <Switch<Route> render={switch} />
                 </main>
-            </BrowserRouter>
+            </HashRouter>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
                 integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
                 crossorigin="anonymous"></script>
