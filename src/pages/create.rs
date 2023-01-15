9f3a54a2fc160a5f9cef2cd0eb3c69f275185c82
Jose Torres <jose@wayve.ai>
2023-01-15 13:43:17 +0000

    Slim down the create component.

--- src/pages/create.rs
+++ src/pages/create.rs
@@ -1,65 +1,21 @@
-use crate::app::Route;
-use crate::components::galaxy::Galaxy;
-use crate::game::Poker;
-use crate::state;
+use crate::{app::Route, game::Poker, hooks::use_app_state, state};
 use gloo::storage::{LocalStorage, Storage};
-use web_sys::HtmlInputElement;
 use yew::prelude::*;
-use yew_router::prelude::use_navigator;
+use yew_router::prelude::Redirect;
 
 #[function_component(Create)]
 pub fn create() -> Html {
-    let navigator = use_navigator().expect("couldn't use the navigator");
+    let app_state = use_app_state();
 
-    let app_state_o = use_state_eq(|| {
-        LocalStorage::get::<state::State>(state::KEY).unwrap_or(state::State::default())
-    });
-    let app_state = app_state_o.clone();
-    let current_name = app_state_o.name.clone();
+    // Create the new game
+    let game = Poker::default();
+    let game_id = game.id;
+    let app_state = app_state.with_game(game);
 
-    let oninput = move |e: InputEvent| {
-        let input: HtmlInputElement = e.target_unchecked_into();
-        let value = input.value();
-
-        app_state.set(app_state.with_name(value));
-    };
-
-    let onsubmit = move |e: SubmitEvent| {
-        let is_bad = match app_state_o.name.clone() {
-            None => true,
-            Some(n) => n.is_empty(),
-        };
-
-        if is_bad {
-            let _ = gloo::utils::window().alert_with_message("Invalid name!");
-            e.prevent_default();
-            return;
-        }
-
-        // Create the new game
-        let game = Poker::default();
-        let game_id = game.id;
-        let new_state = app_state_o.with_game(game);
-
-        // Store the game in the state
-        LocalStorage::set(state::KEY, new_state).expect("failed to persist state");
-        e.prevent_default();
-
-        navigator.push(&Route::Session { id: game_id });
-    };
+    // Store the game in the state
+    LocalStorage::set(state::KEY, app_state).expect("failed to persist state");
 
     html! {
-        <form style="max-width:450px" class="d-flex flex-column m-auto" {onsubmit}>
-            <Galaxy classes={classes!({"align-self-center"})} />
-
-            <div class="mb-5">
-                <label for="name" class="form-label display-6">{"Enter your name to join"}</label>
-                <input type="text" class="form-control form-control-lg" placeholder="Hari Seldon"
-                    required={true} value={current_name} {oninput}
-                />
-            </div>
-
-            <button class="btn btn-info" type="submit">{"Join"}</button>
-        </form>
+        <Redirect<Route> to={Route::Join{ id: game_id }}/>
     }
 }
