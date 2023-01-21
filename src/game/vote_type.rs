8977c1fba7ecac3a1754dbb2769d0fe069bcc832
Jose Torres <jose@wayve.ai>
2023-01-21 16:28:07 +0000

    Add vote type options.

--- src/game/vote_type.rs
+++ src/game/vote_type.rs
@@ -1,15 +1,21 @@
 use serde::{Deserialize, Serialize};
 use strum_macros::{Display, EnumIter};
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, EnumIter, Display)]
+#[derive(Clone, Copy, Debug, EnumIter, Display, PartialEq, Eq, Serialize, Deserialize, Default)]
 pub enum VoteType {
     UpDown,
     FistOfFive,
+    #[default]
     Fibonacci,
 }
 
-impl Default for VoteType {
-    fn default() -> Self {
-        VoteType::Fibonacci
+impl VoteType {
+    #[must_use]
+    pub fn options(&self) -> Vec<i32> {
+        match self {
+            VoteType::UpDown => vec![-1, 0, 1],
+            VoteType::FistOfFive => vec![0, 1, 2, 3, 4, 5],
+            VoteType::Fibonacci => vec![0, 1, 2, 3, 5, 8, 13, 21],
+        }
     }
 }
